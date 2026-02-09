7cfcef383c18d5b37eeca0c6c6fd1976de4e4eda
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-02-08 16:59:41 -0800

    -

--- .sh
+++ .sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
+log=
+clean=
 for i in "$@"; do
 	case "$i" in
-		clean) cargo clean ;;
+		clean) clean=0 ;;
+		log) log=.log ;;
 	esac
 done
-cargo +nightly fmt
-cargo check
-cargo doc \
-	--no-deps #\
-	#--document-private-items
-cargo build
+main() {
+	if [[ -n $clean ]]
+		then cargo clean
+	fi
+	cargo +nightly fmt
+	cargo check
+	cargo doc --no-deps
+	cargo build
+}
+if [[ -z $log ]]
+	then main
+	else main &> $log
+fi
